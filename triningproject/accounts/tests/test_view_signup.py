8ab0e3aae0a73454c062e3e4e0d9cb26fe57fae1
Aga <agnieszkajureczko@o2.pl>
2020-02-10 13:54:38 +0100

    reorganising tests for accounts app

--- triningproject/accounts/tests/test_view_signup.py
+++ triningproject/accounts/tests/test_view_signup.py
@@ -2,8 +2,8 @@ from django.contrib.auth.models import User
 from django.urls import resolve, reverse
 from django.test import TestCase
 
-from .forms import SignUpForm
-from .views import signup
+from ..forms import SignUpForm
+from ..views import signup
 
 
 class SignUpTests(TestCase):
