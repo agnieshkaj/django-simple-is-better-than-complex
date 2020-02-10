66e6ca7ca63950c04d52ea4253f23607c59c3277
Aga <agnieszkajureczko@o2.pl>
2020-02-10 13:17:24 +0100

    creating custom user sign up form

--- triningproject/accounts/tests.py
+++ triningproject/accounts/tests.py
@@ -1,7 +1,8 @@
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
 from django.urls import resolve, reverse
 from django.test import TestCase
+
+from .forms import SignUpForm
 from .views import signup
 
 
@@ -22,7 +23,7 @@ class SignUpTests(TestCase):
 
     def test_contains_form(self):
         form = self.response.context.get('form')
-        self.assertIsInstance(form, UserCreationForm)
+        self.assertIsInstance(form, SignUpForm)
 
 
 class SuccessfulSignUpTests(TestCase):
@@ -30,6 +31,7 @@ class SuccessfulSignUpTests(TestCase):
         url = reverse('signup')
         data = {
             'username': 'john',
+            'email': 'john@doe.com',
             'password1': 'abcdef123456',
             'password2': 'abcdef123456',
         }
@@ -54,6 +56,16 @@ class SuccessfulSignUpTests(TestCase):
         response = self.client.get(self.home_url)
         user = response.context.get('user')
         self.assertTrue(user.is_authenticated)
+
+    def test_form_inputs(self):
+        """
+        The view must contain five inputs: csrf, username, email,
+        password1, password2
+        """
+        self.assertContains(self.response, '<input', 5)
+        self.assertContains(self.response, 'type="text"', 1)
+        self.assertContains(self.response, 'type="email"', 1)
+        self.assertContains(self.response, 'type="password"', 2)
 
 
 class InvalidSignUpTests(TestCase):
