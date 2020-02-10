2c98658a999d91f1e73bf7dc8f5123c979a898f1
Aga <agnieszkajureczko@o2.pl>
2020-02-10 14:37:01 +0100

    creating user actions dropdown menu

--- triningproject/triningproject/urls.py
+++ triningproject/triningproject/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path, re_path
 
 from accounts import views as accounts_views
@@ -22,6 +23,7 @@ from boards import views
 urlpatterns = [
     path('', views.home, name='home'),
     re_path(r'^signup/$', accounts_views.signup, name='signup'),
+    re_path(r'^logout/$', auth_views.LogoutView.as_view(), name='logout'),
     re_path(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
     re_path(r'^boards/(?P<pk>\d+)/new/$', views.new_topic, name='new_topic'),
     path('admin/', admin.site.urls),
